5a34f5cce6f696a132bf705eb2235ef8f7147e45
yTakkar <www.shtakkar@gmail.com>
2017-09-14 14:49:44 +0530

    Added sql file & updated instructions

--- README.md
+++ README.md
@@ -1,20 +1,13 @@
 # Rails-Posts-App
-A Posts/Blog-App created with Rails!!
+A Post-App created with Rails!!
 
 # Usage
-1. Create a MySQL DB [eq. `rails-posts-app`].
+1. Open PHPMyAdmin, create a `DB` named `rails-post-app` & import `db.sql` file.
 2. Create the following environment variable:
 ```javascript
 RAILS_DB_PASSWORD="DB PASSWORD"
 ```
 3. Go to `config/database.yml` file, line no. `19`, and change the socket if yours is different!!
-4. Type following commands.
-```javascript
-rails g model Post title:string content:string
-rake db:migrate
-erb config/database.yml 
-```
-After typing the last command [erb..], check if default's values, if values will be empty, then you've made a mistake!!
 
 5. Start the server
 ```javascript
---
+++ db.sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 14, 2017 at 11:13 AM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rails-posts-app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ar_internal_metadata`
+--
+
+CREATE TABLE `ar_internal_metadata` (
+  `key` varchar(255) NOT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ar_internal_metadata`
+--
+
+INSERT INTO `ar_internal_metadata` (`key`, `value`, `created_at`, `updated_at`) VALUES
+('environment', 'development', '2017-09-13 11:01:54', '2017-09-13 11:01:54');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` bigint(20) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `content` text,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `content`, `created_at`, `updated_at`) VALUES
+(1, 'First Post', 'First Content', '2017-09-13 11:03:36', '2017-09-13 14:18:00'),
+(2, 'Second Title', 'Second Content', '2017-09-13 11:03:55', '2017-09-13 11:03:55');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `schema_migrations`
+--
+
+CREATE TABLE `schema_migrations` (
+  `version` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `schema_migrations`
+--
+
+INSERT INTO `schema_migrations` (`version`) VALUES
+('20170913110049');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `ar_internal_metadata`
+--
+ALTER TABLE `ar_internal_metadata`
+  ADD PRIMARY KEY (`key`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `schema_migrations`
+--
+ALTER TABLE `schema_migrations`
+  ADD PRIMARY KEY (`version`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
